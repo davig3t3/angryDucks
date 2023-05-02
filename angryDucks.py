6f12985938dc6c14e5fb26aa57774e33b124c9db
davig3t3 <davig3t3@gmail.com>
2023-05-02 02:07:30 -0500

    Update angryDucks.py

--- angryDucks.py
+++ angryDucks.py
@@ -1,8 +1,19 @@
 import math
 
+# `PI` and `G` are constants used in the program. `PI` is the mathematical constant pi (approximately
+# equal to 3.14159) and `G` is the acceleration due to gravity (approximately equal to 9.80665 meters
+# per second squared). These constants are used in the calculations to determine the distance the duck
+# will travel after being shot from a gun.
 PI = 3.14159
 G = 9.80665
 
+# This code is an implementation of a program that calculates the distance a duck will travel after
+# being shot from a gun. The program takes in the initial height of the duck, the minimum and maximum
+# distances the duck can be shot, and the number of shots to be taken. For each shot, the program
+# takes in the angle and velocity of the shot and calculates the distance the duck will travel using
+# the given formulas. If the distance is within the minimum and maximum distances, the program prints
+# "DUCK", otherwise it prints "NUCK". The program runs in an infinite loop until an exception is
+# raised (such as the user entering invalid input or pressing Ctrl+C to exit the program).
 while True:
     try:
         h = float(input())
